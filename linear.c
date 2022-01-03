ac2e3475dd043a5d14881fcb6b3fa8568084dfbb
AI & DS <76870716+aiandds@users.noreply.github.com>
2022-01-03 09:32:34 +0530

    Update linear.c

--- linear.c
+++ linear.c
@@ -14,12 +14,18 @@ int main(void)
 {
     int arr[] = { 2, 3, 4, 10, 40 };
     int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = 5
    
     // Function call
     int result = search(arr, n, x);
-    (result == -1)
-        ? printf("Element is not present in array")
-        : printf("Element is present at index %d", result);
+    if (result == -1)
+    {
+        printf("Element is not present in array");
+        return 0;
+    }
+    else
+     printf("Element is present at index %d", result);
+ 
+ 
     return 0;
 }
