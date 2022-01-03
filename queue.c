38c70a7d5854893a5c20c2b0a63b7481469647c7
AI & DS <76870716+aiandds@users.noreply.github.com>
2022-01-03 09:50:43 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,86 +1,104 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-// A linked list (LL) node to store a queue entry
-struct QNode 
-{
-    int key;
-    struct QNode* next;
-};
- 
-// The queue, front stores the front node of LL and rear stores the
-// last node of LL
-struct Queue 
-{
-    struct QNode *front, *rear;
-};
-
-// A utility function to create a new linked list node.
-struct QNode* newNode(int k)
-{
-    struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
-    temp->key = k;
-    temp->next = NULL;
-    return temp;
-}
- 
-// A utility function to create an empty queue
-struct Queue* createQueue()
-{
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = q->rear = NULL;
-    return q;
-}
- 
-// The function to add a key k to q
-void enQueue(struct Queue* q, int k)
-{
-    // Create a new LL node
-    struct QNode* temp = newNode(k);
- 
-    // If queue is empty, then new node is front and rear both
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return;
-    }
- 
-    // Add the new node at the end of queue and change rear
-    q->rear->next = temp;
-    q->rear = temp;
-}
- 
-// Function to remove a key from given queue q
-void deQueue(struct Queue* q)
-{
-    // If queue is empty, return NULL.
-    if (q->front == NULL)
-        return;
- 
-    // Store previous front and move front one node ahead
-    struct QNode* temp = q->front;
- 
-    q->front = q->front->next;
- 
-    // If front becomes NULL, then change rear also as NULL
-    if (q->front == NULL)
-        q->rear = NULL;
- 
-    free(temp);
-}
- 
-// Driver Program to test anove functions
-int main()
-{
-    struct Queue* q = createQueue();
-    enQueue(q, 10);
-    enQueue(q, 20);
-    deQueue(q);
-    deQueue(q);
-    enQueue(q, 30);
-    enQueue(q, 40);
-    enQueue(q, 50);
-    deQueue(q);
-    printf("Queue Front : %d \n", q->front->key);
-    printf("Queue Rear : %d", q->rear->key);
-    return 0;
+#include<stdio.h>   
+#include<stdlib.h>  
+struct node   
+{  
+    int data;   
+    struct node *next;  
+};  
+struct node *front;  
+struct node *rear;   
+void insert();  
+void delete();  
+void display();  
+void main ()  
+{  
+    int choice;   
+    while(choice != 4)   
+    {     
+        printf("\n*************************Main Menu*****************************\n");  
+        printf("\n=================================================================\n");  
+        printf("\n1.insert an element\n2.Delete an element\n3.Display the queue\n4.Exit\n");  
+        printf("\nEnter your choice ?");  
+        scanf("%d",& choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            insert();  
+            break;  
+            case 2:  
+            delete();  
+            break;  
+            case 3:  
+            display();  
+            break;  
+            case 4:  
+            exit(0);  
+            break;  
+            default:   
+            printf("\nEnter valid choice??\n");  
+        }  
+    }  
+}  
+void insert()  
+{  
+    struct node *ptr;  
+    int item;   
+      
+    ptr = (struct node *) malloc (sizeof(struct node));  
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW\n");  
+        return;  
+    }  
+    else  
+    {   
+        printf("\nEnter value?\n");  
+        scanf("%d",&item);  
+        ptr -> data = item;  
+        if(front == NULL)  
+        {  
+            front = ptr;  
+            rear = ptr;   
+            front -> next = NULL;  
+            rear -> next = NULL;  
+        }  
+        else   
+        {  
+            rear -> next = ptr;  
+            rear = ptr;  
+            rear->next = NULL;  
+        }  
+    }  
+}     
+void delete ()  
+{  
+    struct node *ptr;  
+    if(front == NULL)  
+    {  
+        printf("\nUNDERFLOW\n");  
+        return;  
+    }  
+    else   
+    {  
+        ptr = front;  
+        front = front -> next;  
+        free(ptr);  
+    }  
+}  
+void display()  
+{  
+    struct node *ptr;  
+    ptr = front;      
+    if(front == NULL)  
+    {  
+        printf("\nEmpty queue\n");  
+    }  
+    else  
+    {   printf("\nprinting values .....\n");  
+        while(ptr != NULL)   
+        {  
+            printf("\n%d\n",ptr -> data);  
+            ptr = ptr -> next;  
+        }  
+    }  
 }
